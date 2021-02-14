c83ee348121547c959b0c63d63e8d11df605fe96
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2021-02-14 18:13:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ let filterInput = document.getElementById("filterInput");
 filterInput.addEventListener("keyup", filterNames);
 
 function filterNames(){
-    //get value of input
     let filterValue = document.getElementById('filterInput').value.toUpperCase();
-   
-    //get names ul
     let ul = document.getElementById("names");
     let li = ul.querySelectorAll('li.collection-item');
-   // loop through collection-item lis
+ 
    for(let i=0; i<li.length;i++){
        let a = li[i].getElementsByTagName('a')[0];
 
